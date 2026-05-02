a9b33c63b8dc153fa69566c2320d5c4b66d5a7c6
Shalvin P Shaji <shalvinpshaji.official@gmail.com>
2026-05-02 17:54:49 +0530

    feat: Backend skeleton

---
+++ backend/src/main/java/com/fdg/FakeDataGeneratorApplication.java
@@ -0,0 +1,11 @@
+package com.fdg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FakeDataGeneratorApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FakeDataGeneratorApplication.class, args);
+    }
+}
